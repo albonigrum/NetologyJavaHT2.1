6a82237bc55e4a996b1114549bc88ba505ec92a8
Ivan Grebenschikov <albonigrum@gmail.com>
2020-06-05 22:47:04 +0300

    Add MoneyTransfer.java and first version Testing_report_template.md

---
+++ src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int balance = 2_000_000_000;
+        int money_transfer = 500_000_000;
+        int sum = balance + money_transfer;
+        System.out.println(sum);
+    }
+}
